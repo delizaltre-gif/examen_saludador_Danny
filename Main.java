5142b30fa5bc83f90bec38dd7b5bc216b5d034cb
delizaltre-gif <delizaltre1@educacion.navarra.es>
2025-11-14 17:51:59 +0100

    "Cambio saludo en Main"

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo,Soy Danny");
+        System.out.println("Hola mundo,Soy Danny desde la version principal");
         System.out.println("Que tengas un buen dia");
     }
 }
