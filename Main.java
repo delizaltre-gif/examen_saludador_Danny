629e36db85f8d3c29479695656364ed6270ecb84
delizaltre-gif <delizaltre1@educacion.navarra.es>
2025-11-14 17:50:15 +0100

    Añadido mensaje despedida

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo,Soy Danny");
+        System.out.println("Que tengas un buen dia");
     }
 }
