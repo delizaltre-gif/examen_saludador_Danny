6d81a293f3b21913f1e3f89fa57d1498f7317791
delizaltre-gif <delizaltre1@educacion.navarra.es>
2025-11-14 17:53:23 +0100

    "Cambio de saludo en mejoras"

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo,Soy Danny");
+        System.out.println("Hola mundo,Soy Danny desde la rama mejoras");
     }
 }
